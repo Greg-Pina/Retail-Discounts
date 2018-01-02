2df6252444463e47abc416dbc2965f490480a170
Greg Pina <pinagm2825@gmail.com>
2018-01-02 04:40:31 -0500

    Initial commit

---
+++ Retail Discounts/Form1.Designer.vb
@@ -0,0 +1,242 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.BtnCalculate = New System.Windows.Forms.Button()
+        Me.BtnNext = New System.Windows.Forms.Button()
+        Me.BtnExit = New System.Windows.Forms.Button()
+        Me.lbl1 = New System.Windows.Forms.Label()
+        Me.lblYears = New System.Windows.Forms.Label()
+        Me.lblPrevious = New System.Windows.Forms.Label()
+        Me.lblStatus = New System.Windows.Forms.Label()
+        Me.lblTotal = New System.Windows.Forms.Label()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.txtYearsEmployed = New System.Windows.Forms.TextBox()
+        Me.txtPreviousPurchases = New System.Windows.Forms.TextBox()
+        Me.txtTodaysPurchases = New System.Windows.Forms.TextBox()
+        Me.BtnSummary = New System.Windows.Forms.Button()
+        Me.radEmployee = New System.Windows.Forms.RadioButton()
+        Me.radManager = New System.Windows.Forms.RadioButton()
+        Me.lblOutput = New System.Windows.Forms.Label()
+        Me.lblSummary = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'BtnCalculate
+        '
+        Me.BtnCalculate.Location = New System.Drawing.Point(33, 542)
+        Me.BtnCalculate.Name = "BtnCalculate"
+        Me.BtnCalculate.Size = New System.Drawing.Size(114, 23)
+        Me.BtnCalculate.TabIndex = 0
+        Me.BtnCalculate.Text = "Calculate"
+        Me.BtnCalculate.UseVisualStyleBackColor = True
+        '
+        'BtnNext
+        '
+        Me.BtnNext.Location = New System.Drawing.Point(186, 542)
+        Me.BtnNext.Name = "BtnNext"
+        Me.BtnNext.Size = New System.Drawing.Size(114, 23)
+        Me.BtnNext.TabIndex = 1
+        Me.BtnNext.Text = "Next Employee"
+        Me.BtnNext.UseVisualStyleBackColor = True
+        '
+        'BtnExit
+        '
+        Me.BtnExit.Location = New System.Drawing.Point(529, 542)
+        Me.BtnExit.Name = "BtnExit"
+        Me.BtnExit.Size = New System.Drawing.Size(114, 23)
+        Me.BtnExit.TabIndex = 3
+        Me.BtnExit.Text = "Exit"
+        Me.BtnExit.UseVisualStyleBackColor = True
+        '
+        'lbl1
+        '
+        Me.lbl1.AutoSize = True
+        Me.lbl1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl1.Location = New System.Drawing.Point(44, 28)
+        Me.lbl1.Name = "lbl1"
+        Me.lbl1.Size = New System.Drawing.Size(49, 16)
+        Me.lbl1.TabIndex = 4
+        Me.lbl1.Text = "Name"
+        '
+        'lblYears
+        '
+        Me.lblYears.AutoSize = True
+        Me.lblYears.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblYears.Location = New System.Drawing.Point(41, 98)
+        Me.lblYears.Name = "lblYears"
+        Me.lblYears.Size = New System.Drawing.Size(198, 16)
+        Me.lblYears.TabIndex = 5
+        Me.lblYears.Text = "Number of Years Employed"
+        '
+        'lblPrevious
+        '
+        Me.lblPrevious.AutoSize = True
+        Me.lblPrevious.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblPrevious.Location = New System.Drawing.Point(41, 163)
+        Me.lblPrevious.Name = "lblPrevious"
+        Me.lblPrevious.Size = New System.Drawing.Size(258, 16)
+        Me.lblPrevious.TabIndex = 6
+        Me.lblPrevious.Text = "Total Amount of Previous Purchases"
+        '
+        'lblStatus
+        '
+        Me.lblStatus.AutoSize = True
+        Me.lblStatus.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblStatus.Location = New System.Drawing.Point(41, 230)
+        Me.lblStatus.Name = "lblStatus"
+        Me.lblStatus.Size = New System.Drawing.Size(125, 16)
+        Me.lblStatus.TabIndex = 7
+        Me.lblStatus.Text = "Employee Status"
+        '
+        'lblTotal
+        '
+        Me.lblTotal.AutoSize = True
+        Me.lblTotal.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTotal.Location = New System.Drawing.Point(41, 327)
+        Me.lblTotal.Name = "lblTotal"
+        Me.lblTotal.Size = New System.Drawing.Size(191, 16)
+        Me.lblTotal.TabIndex = 8
+        Me.lblTotal.Text = "Total of Today's Purchase"
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(47, 47)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(100, 20)
+        Me.txtName.TabIndex = 9
+        '
+        'txtYearsEmployed
+        '
+        Me.txtYearsEmployed.Location = New System.Drawing.Point(47, 114)
+        Me.txtYearsEmployed.Name = "txtYearsEmployed"
+        Me.txtYearsEmployed.Size = New System.Drawing.Size(100, 20)
+        Me.txtYearsEmployed.TabIndex = 10
+        '
+        'txtPreviousPurchases
+        '
+        Me.txtPreviousPurchases.Location = New System.Drawing.Point(47, 179)
+        Me.txtPreviousPurchases.Name = "txtPreviousPurchases"
+        Me.txtPreviousPurchases.Size = New System.Drawing.Size(100, 20)
+        Me.txtPreviousPurchases.TabIndex = 11
+        '
+        'txtTodaysPurchases
+        '
+        Me.txtTodaysPurchases.Location = New System.Drawing.Point(44, 343)
+        Me.txtTodaysPurchases.Name = "txtTodaysPurchases"
+        Me.txtTodaysPurchases.Size = New System.Drawing.Size(100, 20)
+        Me.txtTodaysPurchases.TabIndex = 13
+        '
+        'BtnSummary
+        '
+        Me.BtnSummary.Location = New System.Drawing.Point(385, 542)
+        Me.BtnSummary.Name = "BtnSummary"
+        Me.BtnSummary.Size = New System.Drawing.Size(114, 23)
+        Me.BtnSummary.TabIndex = 30
+        Me.BtnSummary.Text = "Discount Summary"
+        Me.BtnSummary.UseVisualStyleBackColor = True
+        '
+        'radEmployee
+        '
+        Me.radEmployee.AutoSize = True
+        Me.radEmployee.Location = New System.Drawing.Point(47, 246)
+        Me.radEmployee.Name = "radEmployee"
+        Me.radEmployee.Size = New System.Drawing.Size(71, 17)
+        Me.radEmployee.TabIndex = 31
+        Me.radEmployee.TabStop = True
+        Me.radEmployee.Text = "Employee"
+        Me.radEmployee.UseVisualStyleBackColor = True
+        '
+        'radManager
+        '
+        Me.radManager.AutoSize = True
+        Me.radManager.Location = New System.Drawing.Point(47, 279)
+        Me.radManager.Name = "radManager"
+        Me.radManager.Size = New System.Drawing.Size(67, 17)
+        Me.radManager.TabIndex = 32
+        Me.radManager.TabStop = True
+        Me.radManager.Text = "Manager"
+        Me.radManager.UseVisualStyleBackColor = True
+        '
+        'lblOutput
+        '
+        Me.lblOutput.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblOutput.Location = New System.Drawing.Point(338, 28)
+        Me.lblOutput.Name = "lblOutput"
+        Me.lblOutput.Size = New System.Drawing.Size(341, 218)
+        Me.lblOutput.TabIndex = 33
+        '
+        'lblSummary
+        '
+        Me.lblSummary.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblSummary.Location = New System.Drawing.Point(338, 304)
+        Me.lblSummary.Name = "lblSummary"
+        Me.lblSummary.Size = New System.Drawing.Size(341, 175)
+        Me.lblSummary.TabIndex = 34
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(691, 627)
+        Me.Controls.Add(Me.lblSummary)
+        Me.Controls.Add(Me.lblOutput)
+        Me.Controls.Add(Me.radManager)
+        Me.Controls.Add(Me.radEmployee)
+        Me.Controls.Add(Me.BtnSummary)
+        Me.Controls.Add(Me.txtTodaysPurchases)
+        Me.Controls.Add(Me.txtPreviousPurchases)
+        Me.Controls.Add(Me.txtYearsEmployed)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.lblTotal)
+        Me.Controls.Add(Me.lblStatus)
+        Me.Controls.Add(Me.lblPrevious)
+        Me.Controls.Add(Me.lblYears)
+        Me.Controls.Add(Me.lbl1)
+        Me.Controls.Add(Me.BtnExit)
+        Me.Controls.Add(Me.BtnNext)
+        Me.Controls.Add(Me.BtnCalculate)
+        Me.Name = "Form1"
+        Me.Text = "Employee Discount Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents BtnCalculate As Button
+    Friend WithEvents BtnNext As Button
+    Friend WithEvents BtnExit As Button
+    Friend WithEvents lbl1 As Label
+    Friend WithEvents lblYears As Label
+    Friend WithEvents lblPrevious As Label
+    Friend WithEvents lblStatus As Label
+    Friend WithEvents lblTotal As Label
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents txtYearsEmployed As TextBox
+    Friend WithEvents txtPreviousPurchases As TextBox
+    Friend WithEvents txtTodaysPurchases As TextBox
+    Friend WithEvents BtnSummary As Button
+    Friend WithEvents radEmployee As RadioButton
+    Friend WithEvents radManager As RadioButton
+    Friend WithEvents lblOutput As Label
+    Friend WithEvents lblSummary As Label
+End Class
---
+++ Retail Discounts/Form1.vb
@@ -0,0 +1,226 @@
+﻿'*********************************
+'Greg Pina
+'Midterm Early Submission for Review
+'.Net Programming IT 101-401
+'10/17/2017
+'**********************************
+
+Option Strict On
+Public Class Form1
+    Private Sub BtnExit_Click(sender As Object, e As EventArgs) Handles BtnExit.Click
+
+        'This Closes the Program
+        Close()
+
+    End Sub
+
+    'This keeps our variables outside of any private classes
+    Dim mdblTotalBefore As Double
+    Dim mdblTotalAfter As Double
+    Dim dblPreviousDiscount As Double
+    Dim MaxDiscount As Double
+    Dim mdblTodaysPurchase As Double
+    Dim dblTodaysDiscount As Double
+    Dim dblDiscountPercent As Double
+    Dim mdblTodaysTotal As Double
+
+    'This sets management and employee variables
+    Dim dblManager As Double
+    Dim dblEmployee As Double
+
+    'Variables for Input (left side)
+    Dim dblStatus As Double
+    Dim dblYears As Double
+    Dim dblPreviousPurchases As Double
+    Dim dblDiscount As Double
+    Dim dblYTDDiscount As Double
+
+
+    Private Sub BtnCalculate_Click(sender As Object, e As EventArgs) Handles BtnCalculate.Click
+
+        'This keeps the text boxes looking neat
+        txtName.BackColor = Color.White
+        txtPreviousPurchases.BackColor = Color.White
+        txtTodaysPurchases.BackColor = Color.White
+        txtYearsEmployed.BackColor = Color.White
+
+        Try
+            'This validates the user inputs on the left and makes sure they are correct
+
+            'Correct Name?
+
+            If IsNumeric(txtName.Text) Then
+                MessageBox.Show("Please make sure to enter your name correctly.")
+                txtName.Focus()
+                txtName.BackColor = Color.Yellow
+                Exit Sub
+            End If
+
+            Dim strName = CStr(txtName.Text)
+
+            'Correct Number of Years Employed?
+
+            If IsNumeric(txtYearsEmployed.Text) Then
+                dblYears = CDbl(txtYearsEmployed.Text)
+            Else
+                MessageBox.Show("Please make sure to enter the number of years you've been with us correctly.")
+                txtYearsEmployed.Focus()
+                txtYearsEmployed.BackColor = Color.Yellow
+                Exit Sub
+            End If
+
+
+            'Correct Total Amount of Previous Purchases?
+
+            If IsNumeric(txtPreviousPurchases.Text) Then
+                dblPreviousPurchases = CDbl(txtPreviousPurchases.Text)
+            Else
+                MessageBox.Show("Please make sure to enter amount of previous purchases you've had with us correctly.")
+                txtPreviousPurchases.Focus()
+                txtPreviousPurchases.BackColor = Color.Yellow
+                Exit Sub
+            End If
+
+
+            'This checks employee status and discounts
+            If radEmployee.Checked And
+                 dblYears < 4 Then
+                dblEmployee = 0.1
+            ElseIf radEmployee.Checked And
+                dblYears < 7 Then
+                dblEmployee = 0.14
+            ElseIf radEmployee.Checked And
+                dblYears < 11 Then
+                dblEmployee = 0.2
+            ElseIf radEmployee.Checked And
+                dblYears < 16 Then
+                dblEmployee = 0.25
+            ElseIf radEmployee.Checked And
+                dblYears > 15 Then
+                dblEmployee = 0.3
+            End If
+
+            'This checks manager status and discounts
+            If radManager.Checked And
+                 dblYears < 4 Then
+                dblManager = 0.2
+            ElseIf radManager.Checked And
+                dblYears < 7 Then
+                dblManager = 0.24
+            ElseIf radManager.Checked And
+                dblYears < 11 Then
+                dblManager = 0.3
+            ElseIf radManager.Checked And
+                dblYears < 16 Then
+                dblManager = 0.35
+            ElseIf radManager.Checked And
+                dblYears > 15 Then
+                dblManager = 0.4
+            End If
+
+            'Gets the discount
+            If radEmployee.Checked Then
+                dblDiscount = dblEmployee
+            ElseIf radManager.Checked Then
+                dblDiscount = dblManager
+            End If
+
+            'To display this discount correctly
+            If radManager.Checked Then
+                dblStatus = (dblDiscount * 100)
+            ElseIf radEmployee.Checked Then
+                dblStatus = (dblDiscount * 100)
+            End If
+            'Correct Total of Today's Purchases?
+
+            If IsNumeric(txtTodaysPurchases.Text) Then
+                mdblTodaysPurchase = CDbl(txtTodaysPurchases.Text)
+            Else
+                MessageBox.Show("Please make sure to enter the total of today's purchases with us correctly.")
+                txtYearsEmployed.Focus()
+                txtYearsEmployed.BackColor = Color.Yellow
+                Exit Sub
+            End If
+
+
+            '****************************This is where the actual calculations are done ***********
+
+            'Gets YTD purchase discount
+
+            If (dblPreviousPurchases * dblDiscount) > 200 Then
+                dblYTDDiscount = 200
+            Else
+                dblYTDDiscount = (dblPreviousPurchases * dblDiscount)
+            End If
+            'Gets current purchase discount
+
+            dblTodaysDiscount = (mdblTodaysPurchase * dblDiscount)
+
+            'Todays Total
+            mdblTodaysTotal = mdblTodaysPurchase - dblTodaysDiscount
+
+            mdblTodaysTotal = mdblTodaysPurchase - dblTodaysDiscount
+
+
+            If dblYTDDiscount > 200 Then
+                dblTodaysDiscount = 0
+            Else
+                If dblYTDDiscount + (mdblTodaysPurchase * dblDiscount) > 200 Then
+                    dblTodaysDiscount = 200 - dblYTDDiscount
+                Else
+                    dblTodaysDiscount = (mdblTodaysPurchase * dblDiscount)
+                    Exit Sub
+                End If
+                Exit Sub
+            End If
+
+            '**********************************************************
+
+            'This is the first output box
+
+
+            lblOutput.Text = "Name: " & strName & vbNewLine & vbNewLine & "Employee Discount (%): " & dblStatus & vbNewLine & vbNewLine &
+                "YTD Amount of Discount: " & dblYTDDiscount.ToString("c") & vbNewLine & vbNewLine & "Total Purchase Today Before Discount: " & mdblTodaysPurchase.ToString("c") &
+                vbNewLine & vbNewLine & "Employee Discount This Purchase: " & dblTodaysDiscount.ToString("c") & vbNewLine & vbNewLine & "Total With Discount: " & mdblTodaysTotal.ToString("c")
+
+
+
+
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub BtnNext_Click(sender As Object, e As EventArgs) Handles BtnNext.Click
+
+        'This Moves On to the Next Employee
+
+        'This clears the text boxes on the left
+        txtName.Clear()
+        txtPreviousPurchases.Clear()
+        txtTodaysPurchases.Clear()
+        txtYearsEmployed.Clear()
+
+        'This clears the label on the right
+        lblOutput.Text = String.Empty
+
+        'This puts focus back on the name text box
+        txtName.Focus()
+        Exit Sub
+
+    End Sub
+
+    Private Sub BtnSummary_Click(sender As Object, e As EventArgs)
+
+        'This keeps a running total
+        mdblTotalBefore += mdblTodaysTotal
+        mdblTotalAfter += mdblTodaysPurchase
+
+        'This Displays the Discount Summary
+        lblSummary.Text = "Total before discount for the day: " & mdblTotalBefore.ToString("c") & vbNewLine & "Total after discounts applied: " &
+                mdblTotalAfter.ToString("c")
+    End Sub
+
+
+End Class
